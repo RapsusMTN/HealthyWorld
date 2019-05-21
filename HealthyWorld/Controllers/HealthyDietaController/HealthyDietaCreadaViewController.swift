4b2045a4a2d7c8ae7240a8967062742b9fbeb83b
Jorge Martin <jmartin@fiveflamesmobile.com>
2019-05-21 14:40:12 +0200

    Added new ViewControllers to show the save diet in UDefaults

---
+++ HealthyWorld/Controllers/HealthyDietaController/HealthyDietaCreadaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HealthyDietaCreadaViewController.swift
+//  HealthyWorld
+//
+//  Created by Jorge Martín on 21/05/2019.
+//  Copyright © 2019 Jorge Martin Reyero. All rights reserved.
+//
+
+import UIKit
+
+class HealthyDietaCreadaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
