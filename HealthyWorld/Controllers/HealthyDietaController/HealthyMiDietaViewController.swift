bb1c8614d7e7ce76506227f2e9fb490149ea3d22
Jorge Martin <jmartin@fiveflamesmobile.com>
2019-05-21 14:36:37 +0200

    Create diet in MiDietaViewController and added the alert to save diet in UserDefaults and navigate to new Controller

--- HealthyWorld/Controllers/HealthyDietaController/HealthyMiDietaViewController.swift
+++ HealthyWorld/Controllers/HealthyDietaController/HealthyMiDietaViewController.swift
@@ -12,11 +12,14 @@ class HealthyMiDietaViewController: UIViewController,UITableViewDelegate,UITable
     
     @IBOutlet weak var tableView: UITableView!
     
+    var alimentos:[String] = []
 
+    @IBOutlet weak var kcalTotales: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getAlimentosFromUserDefaults()
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
@@ -26,20 +29,39 @@ class HealthyMiDietaViewController: UIViewController,UITableViewDelegate,UITable
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.alimentos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Celda"
-        
+        cell.textLabel?.text = self.alimentos[indexPath.row]
         
         return cell
     }
     
    
-    
+    func getAlimentosFromUserDefaults() {
+        let defaults = UserDefaults.standard
+        self.alimentos = defaults.object(forKey: "alimentos") as! [String]
+        
+    }
     
 
+    @IBAction func createDieta(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Crear Dieta", message: "¿Desea crear la dieta?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (UIAlertAction) in
+            print("Pressed")
+        }
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default) { (UIAlertAction) in
+            print("Cancelado")
+        }
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
 
 }
